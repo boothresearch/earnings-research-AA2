12decb3741a5e57fe31e4d641698b4bde1d24257
azamasl <aa2821@nyu.edu>
2020-09-15 19:48:00 +0000

    taske 2, error fixed

--- earnings.R
+++ earnings.R
@@ -1,7 +1,7 @@
 earnings <- read.csv("https://raw.githubusercontent.com/avehtari/ROS-Examples/master/Earnings/data/earnings.csv")
 
-singk("reg01.txt", append= FALSE)
+sink("reg01.txt", append= FALSE)
 fitted.model <-lm(earnings$earn ~ earnings$height)
-print(Sys.data(),quote=FALSE)
+print(Sys.time(),quote=FALSE)
 print(summary(fitted.model))
-cloaseAllConnections()
+closeAllConnections()
