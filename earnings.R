5ae68b33b17811f8292cd915e70ad820a7677552
azamasl <aa2821@nyu.edu>
2020-09-15 19:45:10 +0000

    taske 2 is done

--- earnings.R
+++ earnings.R
@@ -1,3 +1,7 @@
 earnings <- read.csv("https://raw.githubusercontent.com/avehtari/ROS-Examples/master/Earnings/data/earnings.csv")
+
+singk("reg01.txt", append= FALSE)
 fitted.model <-lm(earnings$earn ~ earnings$height)
+print(Sys.data(),quote=FALSE)
 print(summary(fitted.model))
+cloaseAllConnections()
